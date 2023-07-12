8ef6524f62e6f9405861f2508414a91cee4f09c3
Danie Leal <112513784+lealjh@users.noreply.github.com>
2023-07-12 18:24:09 -0500

    Commit inicial

---
+++ js/Main.js
@@ -0,0 +1,13 @@
+
+function suma(){
+
+    let A = 0;
+    let B = 0; 
+
+    A = parseInt(prompt("Por favor ingrese el primer valor"));
+    B = parseInt(prompt("Por favor ingrese el segundo valor"));
+ 
+    SUMA = A + B; 
+
+    alert ("La suma es igual a:" + SUMA)
+}
